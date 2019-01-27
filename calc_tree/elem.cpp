d88ff29f661c1ff3bc339e5f055c3c04b86a00d2
BeanLiu <njueebeanliu@hotmail.com>
2019-01-27 12:53:50 +0800

    vs上加入timer后编译一直出错，还没看出原因来。 linux上的编译可以过，但要求(gcc version >= 7)。

--- calc_tree/elem.cpp
+++ calc_tree/elem.cpp
@@ -8,18 +8,18 @@ int main()
 {
 	Timer<chrono::nanoseconds> timer;
 
-	//timer.StartTimer();
+	timer.StartTimer();
 	{
 		Node a(1.56), b(-2);
 		Node n1 = a + b + 3;
 		Node n2 = exp(a) * n1 - abs(b);
 		Node n3 = n1 / n2; 
 	
-		//timer.EndTimer("build time");
-		//timer.StartTimer();
+		timer.EndTimer("build time");
+		timer.StartTimer();
 	
 		cout << n3.Evaluate() << endl;
 	}
-	//timer.EndTimer("evaluate time");
+	timer.EndTimer("evaluate time");
 	return 0;
 }
